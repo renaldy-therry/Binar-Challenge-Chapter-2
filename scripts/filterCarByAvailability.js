394e4d90ddf7eb920d2df1e4a26e1cc590022f4b
Renaldy <renaldytherrydota@gmail.com>
2023-03-15 02:42:44 +0800

    edit filterCarByAvailability

--- scripts/filterCarByAvailability.js
+++ scripts/filterCarByAvailability.js
@@ -4,15 +4,20 @@ function filterCarByAvailability(cars) {
 
   // Tempat penampungan hasil
   const result = [];
-  let available= 0;
+  
 
-  // Tulis code-mu disini
-  for( let i=0; i < cars.length; i++) {
-    if( cars[i].available == true) {
-       result[available] = cars[i];
-       available++;
+  let i = 0;
+  let temp = 0; //angka indeks array result
+
+  do {
+    if( cars[i][`available`] == true) {
+      result[temp] = cars[i];
+      temp = temp + 1;
     }
-  }
+    i++;
+  } while (i < cars.length);
+
+ 
    
   // Rubah code ini dengan array hasil filter berdasarkan availablity
   return result;
