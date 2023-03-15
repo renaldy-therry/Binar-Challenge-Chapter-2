598bf4eec1af6288a1cd23d0d6f69bafbbfe5eb4
Renaldy <renaldytherrydota@gmail.com>
2023-03-15 11:00:04 +0800

    edit sortCarByYearAscendingly

--- scripts/sortCarByYearAscendingly.js
+++ scripts/sortCarByYearAscendingly.js
@@ -6,15 +6,20 @@ function sortCarByYearAscendingly(cars) {
   // Apa itu side effect?
   const result = [...cars];
 
-  // Tulis code-mu disini
-  for(let i=0; i< result.length; i++) {
-    for(let j=0; j < result.length-1; j++){
-      if (result[j].year > result[j+1].year) {
-        let temp = result[j];
-        result[j] = result[j+1];
-        result[j+1] = temp;
+for (let i = 0; i < result.length - 1; ++i) {
+    let swapped = 0;
+      for (let j = 0; j < result.length - i - 1; ++j) {
+        if (result[j].year > result[j+1].year) {
+          let temp = result[j];
+          result[j] = result[j+1];
+          result[j+1] = temp;
+
+          swapped = 1;
+        } 
       }
-    }
+      if(swapped == 0) {
+        break;
+      }
   }
 
   // Rubah code ini dengan array hasil sorting secara ascending
